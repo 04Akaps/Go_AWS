fcdd26b35dd2f2e8b250301f28a94c5cbca65fea
유호진 <hojin.yu@supercat.co.kr>
2023-03-17 10:18:49 +0900

    (feat) sqs 수신에 대한 기본 개념 주석 추가

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os"
-	"time"
 
 	"github.com/jjimgo/Go_AWS/goConfig"
 	"github.com/jjimgo/Go_AWS/goaws"
@@ -23,30 +20,32 @@ func init() {
 }
 
 func main() {
-	t := time.Now()
-	startTime := t.Format("2006-01-02 15:04:05")
-	logFile, err := os.Create("serverLog/server_" + startTime + ".log")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logFile.Close()
+	// 서버 로그 처리에 대해서 고민하다가 aws와 같은 방식으로 적용한 서버에 대한 로그 처리 채널
+	// aws폴더에 있는 커스텀하게 작성한 interface를 사용하면 더 깔끔하게 작성 가능
+	// t := time.Now()
+	// startTime := t.Format("2006-01-02 15:04:05")
+	// logFile, err := os.Create("serverLog/server_" + startTime + ".log")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer logFile.Close()
 
-	logger := log.New(logFile, "", log.LstdFlags)
+	// logger := log.New(logFile, "", log.LstdFlags)
 
-	errChannel := make(chan error)
+	// errChannel := make(chan error)
 
-	go func() {
-		for {
-			select {
-			case err := <-errChannel:
-				logger.Println(err)
-			}
-		}
-	}()
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case err := <-errChannel:
+	// 			logger.Println(err)
+	// 		}
+	// 	}
+	// }()
 
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		errChannel <- err
-	}
+	// if err := http.ListenAndServe(":80", nil); err != nil {
+	// 	errChannel <- err
+	// }
 
 	awsSession.SendMessageToSQS("golangEventQueue", "my Test Message")
 }
